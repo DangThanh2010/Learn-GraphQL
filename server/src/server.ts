d9eae0b1715df3daab8f43bf894f7b0688f41062
ThanhDang2010 <dangminhthanh1412@gmail.com>
2023-12-09 13:03:54 +0700

    Setup graphql for server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,28 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
 import express from "express";
+import { readFile } from "fs/promises";
 import { authMiddleware, handleLogin } from "./auth";
+import { resolvers } from "./graphql/resolvers";
 
 const PORT = 9000;
 
 const app = express();
 app.use(cors(), express.json(), authMiddleware);
 
-app.get("/login", (req, res) => {
-  return res.json({
-    text: "11111",
-  });
-});
 app.post("/login", handleLogin);
 
-app.listen({ port: PORT }, () => {
-  console.log(`Server running on port ${PORT}`);
+//Appolo server for graphql
+readFile("./src/graphql/schema.graphql", "utf-8").then((typeDefs) => {
+  const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+  apolloServer.start().then(() => {
+    app.use("/graphql", expressMiddleware(apolloServer));
+
+    app.listen({ port: PORT }, () => {
+      console.log(`Server running on port ${PORT}`);
+      console.log(`Graphql Server running on port ${PORT}`);
+    });
+  });
 });
