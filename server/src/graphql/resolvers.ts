c1cfa36557afc62c3db9b97c7ee726e7e731ebda
ThanhDang2010 <dangminhthanh1412@gmail.com>
2024-01-06 16:32:06 +0700

    add get job and company by id

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,11 +1,23 @@
 import { Job } from "../@type/job";
 import { getCompanies, getCompany } from "../db/companies";
-import { getJobs } from "../db/jobs";
+import { getJob, getJobs } from "../db/jobs";
 
 export const resolvers = {
   Query: {
     companies: () => getCompanies(),
+    company: (_root: any, args: { id: string }) => {
+      if (args.id) {
+        return getCompany(args.id);
+      }
+      return null;
+    },
     jobs: () => getJobs(),
+    job: (_root: any, args: { id: string }) => {
+      if (args.id) {
+        return getJob(args.id);
+      }
+      return null;
+    },
   },
 
   Job: {
